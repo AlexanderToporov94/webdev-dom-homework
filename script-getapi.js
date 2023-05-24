f9f339f64fa0e7776e6d6ca31a7537b6ff0dab64
AlexanderToporov94 <tav003@mail.ru>
2023-05-24 22:19:52 +0300

    add modul getapi-script

---
+++ script-getapi.js
@@ -0,0 +1,28 @@
+export let userComments = [];
+
+export function getApi () {
+    return fetch("https://webdev-hw-api.vercel.app/api/v1/alex-toporov/comments", {
+      method: "GET"
+    })
+  
+    .then((response) => {
+      return response.json();
+    })
+  
+    .then((responseData) => {
+      const appComments = responseData.comments.map((comment) => {
+        return {
+          name: comment.author.name,
+          date: new Date(comment.date).toLocaleString().slice(0,-3),
+          text: comment.text,
+          likes: comment.likes,
+          isLiked: false,
+        };
+      });
+      userComments = appComments;
+      console.log(userComments);
+      renderComments();
+    })
+}
+
+getApi();
